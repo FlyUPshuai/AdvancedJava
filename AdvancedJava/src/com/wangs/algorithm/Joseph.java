ff727405726b98ce6848e7ef3ce6740842ac9c20
shuai wang <wang@shuaideMacBook-Pro.local>
2016-09-07 15:40:41 +0800

    explain

--- AdvancedJava/src/com/wangs/algorithm/Joseph.java
+++ AdvancedJava/src/com/wangs/algorithm/Joseph.java
@@ -3,6 +3,11 @@ package com.wangs.algorithm;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 约瑟夫环
+ * @author wang
+ *
+ */
 public class Joseph {
 	public static void main(String[] args) {
 		List<Integer> list =new ArrayList<>();
@@ -11,7 +16,13 @@ public class Joseph {
 		}
 		System.out.println("约瑟夫环的值为："+joseph(list,1,3).get(0));
 	}
-	
+	/**
+	 * 
+	 * @param list 全部的人
+	 * @param number1 计数变量
+	 * @param number2 约瑟夫环指数
+	 * @return
+	 */
 	public static List<Integer> joseph(List<Integer> list,int number1,int number2) {
 		if(list.size()>1){
 			if(number1%number2==0){
